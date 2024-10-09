c800b336c11af9d76fcc673ae3bfb95a60638da2
kuraga-2 <denismanarskij@tutamail.com>
2024-10-09 12:00:19 +0300

    z_10 nedk

--- z_10.py
+++ z_10.py
@@ -4,11 +4,11 @@ data = data.split()
 a = int(data[0])
 b = int(data[1])
 c = int(data[2])
-if c == a *b:
+d = int(data[3])
+for i in range(-100,101):
+    if a*(i^3) + b*(i^2) + c*i + d == 0:
+      break
     output_data = open('output.txt', 'w')
-    output_data.write("YES")
-else:
-    output_data = open('output.txt', 'w')
-    output_data.write("NO")
+    output_data.write(str(i))
 input_data.close()
 output_data.close()
