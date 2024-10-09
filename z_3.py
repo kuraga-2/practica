30b8b62b30ed4495e1b75de647fda70a9f0ee88f
kuraga-2 <denismanarskij@tutamail.com>
2024-10-09 11:14:17 +0300

    z_3

---
+++ z_3.py
@@ -0,0 +1,7 @@
+input_data = open('input.txt', 'r')
+data = input_data.read()
+a = int(data)
+output_data = open('output.txt', 'w')
+output_data.write(str(a*a))
+input_data.close()
+output_data.close()
